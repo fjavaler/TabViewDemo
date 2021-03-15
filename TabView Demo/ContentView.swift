cbb77cd87a69259841aecb4cae6322f3905564c2
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-15 11:47:06 -0700

    Initial Commit

---
+++ TabView Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabView Demo
+//
+//  Created by Frederick Javalera on 3/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabView Demo/TabView_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabView_DemoApp.swift
+//  TabView Demo
+//
+//  Created by Frederick Javalera on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct TabView_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
