28bf3cf2a92b8f14d23b0d14fd73163de5561a72
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-31 20:22:49 -0600

    Finished.

--- TabView Demo/ContentView.swift
+++ TabView Demo/ContentView.swift
@@ -8,76 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        TabView {
-            Text("Home Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Home")
-                }
-            
-            Text("Bookmark Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "bookmark.circle.fill")
-                    Text("Bookmark")
-                }
-            
-            Text("Video Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "video.circle.fill")
-                    Text("Video")
-                }
-            
-            Text("Profile Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("Profile")
-                }
-            
-            Text("Profile Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("Profile")
-                }
-            
-            Text("Profile Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("Profile")
-                }
-            
-            Text("Profile Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("Profile")
-                }
-            
-            Text("Profile Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("Profile")
-                }
-            
-            Text("Profile Tab")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("Profile")
-                }
+  
+  //Specifies which tab to launch at. Will launch at tab with .tag(2) below.
+  @State var tabIndex = 2
+  
+  var body: some View {
+    TabView (selection: $tabIndex){
+      Text("Home Tab")
+        .font(.system(size: 30, weight: .bold, design: .rounded))
+        .tabItem {
+          VStack {
+            Image(systemName: "house.fill")
+            Text("Home")
+          }
         }
+        .tag(0)
+      
+      Text("Bookmark Tab")
+        .font(.system(size: 30, weight: .bold, design: .rounded))
+        .tabItem {
+          VStack {
+            Image(systemName: "bookmark.circle.fill")
+            Text("Bookmark")
+          }
+        }
+        .tag(1)
+      
+      Text("Video Tab")
+        .font(.system(size: 30, weight: .bold, design: .rounded))
+        .tabItem {
+          VStack {
+            Image(systemName: "video.circle.fill")
+            Text("Video")
+          }
+        }
+        .tag(2)
+      
+      Text("Profile Tab")
+        .font(.system(size: 30, weight: .bold, design: .rounded))
+        .tabItem {
+          VStack {
+            Image(systemName: "person.crop.circle")
+            Text("Profile")
+          }
+        }
+        .tag(3)
+      
+      Text("Profile Tab")
+        .font(.system(size: 30, weight: .bold, design: .rounded))
+        .tabItem {
+          VStack {
+            Image(systemName: "person.crop.circle")
+            Text("Profile")
+          }
+        }
+        .tag(4)
+      
+      Text("Profile Tab")
+        .font(.system(size: 30, weight: .bold, design: .rounded))
+        .tabItem {
+          VStack {
+            Image(systemName: "person.crop.circle")
+            Text("Profile")
+          }
+        }
+        .tag(5)
+      
+      Text("Profile Tab")
+        .font(.system(size: 30, weight: .bold, design: .rounded))
+        .tabItem {
+          VStack {
+            Image(systemName: "person.crop.circle")
+            Text("Profile")
+          }
+        }
+        .tag(6)
+      
+      Text("Profile Tab")
+        .font(.system(size: 30, weight: .bold, design: .rounded))
+        .tabItem {
+          VStack {
+            Image(systemName: "person.crop.circle")
+            Text("Profile")
+          }
+        }
+        .tag(7)
+      
+      Text("Profile Tab")
+        .font(.system(size: 30, weight: .bold, design: .rounded))
+        .tabItem {
+          VStack {
+            Image(systemName: "person.crop.circle")
+            Text("Profile")
+          }
+        }
+        .tag(8)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
